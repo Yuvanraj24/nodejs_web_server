456d98cb86325936f64268c960ba0e2ad78b91c7
Yuvanraj <itsmeyuvanraj@gmail.com>
2024-02-06 23:43:59 +0530

    error handler working

--- web_server.js
+++ web_server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express ()
 const path = require('path')
+const cors = require('cors')
 const {logger} = require('./middleware/logEvents')
 const PORT = process.env.PORT || 2426;
 
 app.use(logger)
+
+
+//Cross Orgin Resource Sharing
+const whitelist = ['http://localhost:2426']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1 || !origin){
+            callback(null,true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
 
 app.use(express.urlencoded({extended:false}))
 
