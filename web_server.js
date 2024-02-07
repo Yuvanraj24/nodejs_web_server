7dfc4741a394dae903ff4f94ab61ddcb5d2565a8
yuvanraj <yuvanraj.a@kamionscs.com>
2024-02-07 18:42:27 +0530

    learning routes

--- web_server.js
+++ web_server.js
@@ -3,6 +3,7 @@ const app = express ()
 const path = require('path')
 const cors = require('cors')
 const {logger} = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 2426;
 
 app.use(logger)
@@ -22,55 +23,26 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-
-
 app.use(express.urlencoded({extended:false}))
-
 app.use(express.json())
-
 app.use(express.static(path.join(__dirname,'./public')))
 
-app.get('^/$|index(.html)?',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','index.html')); 
+app.use('/', require('./routes/root'))
+app.use('/subdir', require('./routes/subdir'))
+
+app.all('*', (req,res) => {
+    
+    res.status(404);
+
+    if(req.accepts ('html')){
+        res.sendFile(path.join(__dirname,'views','404.html'));
+    } else if(req.accepted('json')){
+        res.json({"error": "404 Not Found"});
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 })
 
-app.get('/new-page(.html)?',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','new-page.html')); 
-})
+app.use(errorHandler)
 
-app.get('/old-page(.html)?',(req,res)=>{
-    res.redirect(301,'new-page.html'); 
-})
-
-app.get('/hello(.html)?',(req,res,next)=> {
-    console.log('hello.html page is loading...')
-    next()
-}, (req, res)=> {
-    res.send('This is Yuvanraj')
-})
-
-
-const one = (req,res,next) => {
-    console.log('one')
-    next()
-}
-const two = (req,res,next) => {
-    console.log('two')
-    next()
-}
-
-const three = (req,res,next) => {
-    console.log('three')
-    res.send('Finished..!')
-}
-
-app.get('/chain(.html)?',[one,two,three])
-
-
-app.get('/*',(req,res)=> {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-})
-
-app.listen(PORT,()=>{
-    console.log(`The Server is running on port --> ${PORT}`);
-})
+app.listen(PORT, ()=> console.log(`The Server is running on port --> ${PORT}`))
